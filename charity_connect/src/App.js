fa200d020e30828c14a4052005ab4c872c3556a0
Anna Tran <annatm268@gmail.com>
2021-08-28 19:49:40 -0400

    added dropdown

--- charity_connect/src/App.js
+++ charity_connect/src/App.js
@@ -1,11 +1,13 @@
-
-
+import React from 'react';
 import Search from './components/search';
-import Group from './assets/Group.png'
+import Group from './assets/Group.png';
+import Dropdown from './components/dropdown';
+
 
 
 function App() {
   return (
+    
     <div className="App">
       <div className="App-header">
         <div className="title">
@@ -17,11 +19,21 @@ function App() {
           Supporting marginalized communities by connecting them with 50+ relevant organizations and services in Waterloo Region
           </text>
         </div>
+        <div>
         <Search/>
-
+        </div>
+        <div className="or">
+          <text>
+          Or select a service you are looking for:
+          </text>
+        </div>
+        <div className="drop">
+        <Dropdown/>
+        </div>
       </div>
       <img className="graphic" src={Group} />
     </div>
+    
   );
 }
 
